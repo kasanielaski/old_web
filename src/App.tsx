67f477b97d5f2dd7b62fef8784a52172851b1719
Sergey Shapirenko <sergey.shapirenko@invent.us>
2025-11-25 23:40:45 +0700

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,66 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <marquee scrollamount="5" behavior="alternate">
+        🌟🌟 WELCOME TO MY PERSONAL WEBSITE 🌟🌟
+      </marquee>
+
+      <div className="container">
+        <h1 className="title">
+          Hi, I'm <blink>KASANIE_LASKI</blink>
+        </h1>
+
+        <img className="construction" src="construction.gif" />
+
+        <table cellSpacing="0" cellPadding="12">
+          <tr>
+            <td className="nav">
+              <a href="#">HOME</a> |<a href="#">ABOUT ME</a> |
+              <a href="#">PROJECTS</a> |<a href="#">GUESTBOOK</a> |
+              <a href="#">COOL LINKS</a>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <h2>About Me</h2>
+              <p>
+                Hello traveler of the <b>World Wide Web</b>! This website is
+                best viewed in <u>Netscape Navigator</u>
+                at 800×600 resolution.
+              </p>
+
+              <h2>Cool Stuff</h2>
+              <ul>
+                <li>🔥 Animated GIFs</li>
+                <li>💾 Zip downloads</li>
+                <li>🛸 Sci-fi fonts</li>
+                <li>🎵 Optional MIDI music</li>
+              </ul>
+
+              <h2>My Badges</h2>
+              <img src="https://i.ibb.co/PMtw3bP/best-viewed.gif" />
+              <img src="https://i.ibb.co/5nF6dHt/netscape.gif" />
+              <img src="https://i.ibb.co/W6p8m7c/ie.gif" />
+            </td>
+          </tr>
+        </table>
+
+        <center>
+          <h3>Visitor Counter:</h3>
+          <img src="https://i.ibb.co/Lg7fXj7/counter.gif" />
+        </center>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <marquee scrollamount="6">
+        <img src="https://i.ibb.co/4Y4tKkw/flame.gif" />
+        <img src="https://i.ibb.co/4Y4tKkw/flame.gif" />
+        <img src="https://i.ibb.co/4Y4tKkw/flame.gif" />
+      </marquee>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
